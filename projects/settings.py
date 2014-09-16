a57a821931b326741e20227ef6bf7edaae9234c8
jhanes <jhanes@meraki-inv.(none)>
2014-09-16 17:24:42 -0400

    ....forgot settings.py.

--- projects/settings.py
+++ projects/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_DIR = os.path.join('/home/projects/meraki_inv/')
+PROJECT_DIR = os.path.join('/home/meraki/meraki_inv/')
 
 
 # Quick-start development settings - unsuitable for production
